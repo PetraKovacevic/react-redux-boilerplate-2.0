3d3409e4e390704c2dbe1be7e005f091250e4c81
Steve Kane <steve@stevekane.com.au>
2017-05-30 19:24:29 +1000

    Config update

--- src/config.js
+++ src/config.js
@@ -1,24 +1,12 @@
+/**
+ * Global application config
+ * Other configurations will live with their components / services
+ */
+
 const config = {
     appName: 'React BoilerPlate',
     language: 'en', // en, es, fr, de, etc... Found in strings.js
-    pageTitleSeparator: ' | ',
-    apiEndpoint: 'http://fill-me-in.com',
-    jwtType: 'aws_cognito',
-    awsUserPoolId: 'ap-southeast-2_Dw7s9JN9Q',
-    awsClientId: '22j045c5blmrg4ibsgfdrp0u58',
-    defaultRedirect: '/my-details',
-    spinnerConfig: {
-        lines: 11,
-        length: 20,
-        width: 20,
-        radius: 6,
-        scale: 0.75,
-        color: 'rgba(0, 151, 191 .25)',
-        speed: 1,
-        className: 'spinner',
-        hwaccell: true,
-        top: '40%'
-    }
+    pageTitleSeparator: ' | '
 };
 
 export default config;
