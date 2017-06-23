59581c0dfaa378741f05c90ddd6c97becf606bdf
Steve Kane <steve@stevekane.com.au>
2017-06-23 11:45:10 +1000

    moved reducer combining into the store setup to save a file

--- src/store.js
+++ src/store.js
@@ -1,7 +1,17 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import ReduxThunk from 'redux-thunk';
-import reducers from '@/reducers';
+import { reducer as formReducer } from 'redux-form';
+import { reducer as servicesReducer } from '@/services/reducer';
+import { reducer as signReducer } from '@/scenes/Sign/scenes/reducer';
+import { reducer as dataReducer } from '@/data/reducer';
+
+const reducers = combineReducers({
+    form: formReducer,
+    services: servicesReducer,
+    sign: signReducer,
+    data: dataReducer
+});
 
 const composeEnhancers = composeWithDevTools({});
 const store = createStore(reducers, composeEnhancers(applyMiddleware(ReduxThunk)));
