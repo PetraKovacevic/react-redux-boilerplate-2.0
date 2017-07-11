890c8c14b3621eaf45a09459479e61eadbc4ed47
Petra Kovacevic <petra.kovacevic@4mation.com.au>
2017-07-11 18:36:02 +1000

    Added tests for the log out button. Added Snapshot

--- test/jestsetup.js
+++ test/jestsetup.js
@@ -1,5 +1,9 @@
 // Make Enzyme functions available in all test files without importing
 import { shallow, render, mount } from 'enzyme';
+// handle taps and clicks nicely
+import injectTapEventPlugin from 'react-tap-event-plugin';
+
+injectTapEventPlugin();
 global.shallow = shallow;
 global.render = render;
 global.mount = mount;
